6683e2eea7491064c0281f7fe89875538cd8cbb2
EVERTON B WEBB <Ev@EVERTONs-MacBook-Pro.local>
2017-05-13 12:49:44 -0400

    working on post routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,19 +6,19 @@ router.get('/', function (req, res) {
     queries.show(function(data){
         var data1 = {
             burgerData: data
-        }
+        };
         res.render('index', data1);
     });
 });
 
 router.post('/create', function (req, res) {
-    queries.add(req.body.item, function(data) {
+    queries.add(req.body.item, function() {
         res.redirect('/');
     });
 });
 
 router.post('/update', function(req, res){
-    queries.eat(req.body.id, function(result){
+    queries.eat(req.body.id, function(){
         res.redirect('/');
     });
 });
